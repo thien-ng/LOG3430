27ffbeb64050b13f29e78dea4ca018a1b588a5b6
Thienng97 <duc-thien@sympatico.ca>
2019-09-11 17:24:26 -0400

    change exception assert

--- TP1/test_services.py
+++ TP1/test_services.py
@@ -4,7 +4,7 @@ from unittest.mock import Mock
 import os
 from models import Contact
 from DAOs import ContactDAO
-from services import ContactService
+from services import *
 from datetime import datetime
 
 # To complete...
@@ -27,11 +27,8 @@ class TestContactService(unittest.TestCase):
 
     def test_when_contact_is_created_and_DAO_get_by_names_returns_contact_it_should_raise_AlreadyExistedItem(self):
         self.contactDAO.get_by_names.return_value = "SOMEBODY"
-        try:
-            self.contactService.create_contact('Houssem','Ben Braiek','123-456-7891','houssem.bb@gmail.com')
-        except Exception as e:
-            self.assertEqual(str(e), "Contact with first name <Houssem> and last name <Ben Braiek> already exist")
-    
+        self.assertRaises(AlreadyExistedItem, self.contactService.create_contact, 'Houssem','Ben Braiek','123-456-7891','houssem.bb@gmail.com')
+
     def test_when_contact_is_changed_updated_should_be_True(self):
         contact = self.contactService.update_contact('id','Houssem','Ben Braiek','123-456-7891','houssem.bb@gmail.com')
         self.assertTrue(contact.updated)
@@ -42,10 +39,7 @@ class TestContactService(unittest.TestCase):
     
     def test_when_contact_is_changed_and_DAO_update_returns_zero_it_should_raise_UndefinedID(self):
         self.contactDAO.update.return_value = 0
-        try:
-            self.contactService.update_contact('id','Houssem','Ben Braiek','123-456-7891','houssem.bb@gmail.com')
-        except Exception as e:
-            self.assertEqual(str(e), "No contact with id <id>")
+        self.assertRaises(UndefinedID, self.contactService.update_contact, 'id','Houssem','Ben Braiek','123-456-7891','houssem.bb@gmail.com')
 
     def test_when_retrieve_contact_is_called_with_id_and_DAO_get_by_id_should_be_called(self):
         self.contactService.retrieve_contact(id='id')
@@ -57,17 +51,11 @@ class TestContactService(unittest.TestCase):
 
     def test_when_retrieve_contact_is_called_with_id_and_DAO_returns_None_it_should_raise_UndefinedID(self):
         self.contactDAO.get_by_id.return_value = None
-        try:
-            self.contactService.retrieve_contact(id='id')
-        except Exception as e:
-            self.assertEqual(str(e), "No contact with id <id>")
+        self.assertRaises(UndefinedID, self.contactService.retrieve_contact, id='id')
     
     def test_when_retrieve_contact_is_called_with_names_and_DAO_returns_None_it_should_raise_NotExistedItem(self):
         self.contactDAO.get_by_names.return_value = None
-        try:
-            self.contactService.retrieve_contact(first_name='firstname', last_name='lastname')
-        except Exception as e:
-            self.assertEqual(str(e), "No contact with first name <firstname> and last name <lastname>")
+        self.assertRaises(NotExistedItem, self.contactService.retrieve_contact, first_name='firstname', last_name='lastname')
 
     def test_when_delete_contact_is_called_with_id_and_DAO_delete_by_id_should_be_called(self):
         self.contactService.delete_contact(id='id')
@@ -79,17 +67,11 @@ class TestContactService(unittest.TestCase):
 
     def test_when_delete_contact_is_called_with_id_and_DAO_delete_by_id_returns_zero_it_should_raise_UndefinedID(self):
         self.contactDAO.delete_by_id.return_value = 0
-        try:
-            self.contactService.delete_contact(id='id')
-        except Exception as e:
-            self.assertEqual(str(e), "No contact with id <id>")
+        self.assertRaises(UndefinedID, self.contactService.delete_contact, id='id')
     
     def test_when_retrieve_contact_is_called_with_names_and_DAO_delete_by_names_returns_zero_it_should_raise_NotExistedItem(self):
         self.contactDAO.delete_by_names.return_value = 0
-        try:
-            self.contactService.delete_contact(first_name='firstname', last_name='lastname')
-        except Exception as e:
-            self.assertEqual(str(e), "No contact with first name <firstname> and last name <lastname>")
+        self.assertRaises(NotExistedItem, self.contactService.delete_contact, first_name='firstname', last_name='lastname')
     
 
     
