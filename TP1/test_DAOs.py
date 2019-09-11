51bc8867b0671138df64b8b30534fc958be6a52e
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-09-11 15:35:14 -0400

    f

--- TP1/test_DAOs.py
+++ TP1/test_DAOs.py
@@ -46,7 +46,7 @@ class TestContactDAO(unittest.TestCase):
     
     def test_after_deleting_contact_by_id_get_it_with_id_should_return_None(self):
         pass
-
+    
     def test_deleting_undefined_id_should_return_zero(self):
         pass
 
@@ -58,7 +58,7 @@ class TestContactDAO(unittest.TestCase):
 
     def test_update_contact_should_set_the_provided_values(self):
         pass
-
+    ##########################################
     def test_update_contact_should_return_zero_if_id_does_not_exist(self):
         pass
 
