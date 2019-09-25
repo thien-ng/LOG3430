f743e972cd00faa15514cede4cd9e919a01fb9bd
thien <d.thien.ng97@gmail.com>
2019-09-25 16:33:21 -0400

    add runnable shit

---
+++ TP2/Graph App/generators_blackbox.py
@@ -0,0 +1,12 @@
+import unittest
+import models
+
+class TestGeneratorsBlackBox(unittest.TestCase):
+
+    def setUp(self):
+        print("test")
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
