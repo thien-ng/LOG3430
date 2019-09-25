bbba228ea13b1f12aa41bcb7559307965915d3ee
thien <d.thien.ng97@gmail.com>
2019-09-25 16:40:20 -0400

    initial commit

--- TP2/Graph App/generators_blackbox.py
+++ TP2/Graph App/generators_blackbox.py
@@ -1,11 +1,11 @@
+from generators import *
 import unittest
 import models
 
 class TestGeneratorsBlackBox(unittest.TestCase):
-
+    
     def setUp(self):
         print("test")
-
 
 if __name__ == '__main__':
     unittest.main()
