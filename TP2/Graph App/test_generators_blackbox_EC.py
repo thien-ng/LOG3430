1935adb82eb0f57e6d366bf38740a942dab77ab1
thien <d.thien.ng97@gmail.com>
2019-10-02 16:47:18 -0400

    initial commit

--- TP2/Graph App/test_generators_blackbox_EC.py
+++ TP2/Graph App/test_generators_blackbox_EC.py
@@ -2,7 +2,7 @@ from generators import *
 import unittest
 import models
 
-class TestEulerianCycle(unittest.TestCase):
+class TestEulerianCycleEC(unittest.TestCase):
     
     def test_when_generate_eulerian_cycle_with_negative_vertex_should_raise_ValueError(self):
         self.assertRaises(ValueError, eulerianCycle, -5, 2)
