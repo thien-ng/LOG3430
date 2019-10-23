10fec4e363a4c34ac369e2ce0e9d2a634bd7cb22
thien <d.thien.ng97@gmail.com>
2019-10-23 16:35:15 -0400

    initial commit

--- TP3/test_Queue.py
+++ TP3/test_Queue.py
@@ -1,6 +1,31 @@
+from Queue import *
 import unittest
+
+class testNode(unittest.TestCase):
+
+    def test_value_constructor_init(self):
+        node = Node("testValue")
+
+        self.assertEquals(node.value, "testValue")
+
+    def test_value_autre_str(self):
+        node = Node(1)
+
+        self.assertEquals(node.__str__(), "1")
+
+    def test_next_constructor_init(self):
+        node = Node("testValue")
+
+        self.assertIsNone(node.next);
 
 class TestQueue(unittest.TestCase):
 
-    def setUp(self):
-        print("test")
+    def test_first_rapporteur_checkFirst(self):
+        queue = Queue();
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+  
