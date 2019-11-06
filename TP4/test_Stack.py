79427cd5d27530ac077802e0b9738e2f0ada5573
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-11-06 18:21:18 -0500

    initial commit

--- TP4/test_Stack.py
+++ TP4/test_Stack.py
@@ -3,5 +3,43 @@ import unittest
 
 class TestStack(unittest.TestCase):
 
-    def test(self):
-        print("test")
+    def test_sequence_1_vide_vide(self):
+        stack = Stack(100, 100, 10)
+        self.assertRaises(ValueError, stack.pop)
+
+    def test_sequence_2_vide_partiellement_plein(self):
+        stack = Stack(100, 100, 10)
+        stack.push("node")
+        item = stack.pop()
+
+        self.assertEqual(item, "node")
+        self.assertIsNone(stack.first)
+        self.assertEqual(stack.n, 0)
+
+    def test_sequence_3_vide_partiellement_plein(self):
+        stack = Stack(100, 100, 10)
+        stack.push("node1")
+        stack.push("node2")
+
+        self.assertEqual(stack.first.value, "node2")
+        self.assertEqual(stack.n, 2)
+
+    def test_sequence_4_vide_partiellement_plein(self):
+        stack = Stack(2, 5, 10)
+        stack.push("node1")
+        stack.push("node2")
+        
+        item = stack.pop()
+        self.assertEqual(item, "node2")
+        self.assertEqual(stack.first.value, "node1")
+        self.assertEqual(stack.n, 1)
+        
+    def test_sequence_5_vide_plein(self):
+        stack = Stack(2, 5, 10)
+        stack.push("node1")
+        stack.push("node2")
+
+        self.assertTrue(stack.isFull())
+    
+if __name__ == '__main__':
+    unittest.main()
