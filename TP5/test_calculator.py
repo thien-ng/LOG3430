59d0e1eb738c7896c4ab209429f4c9c3c56bcfe6
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-11-20 18:28:12 -0500

    initial commit

--- TP5/test_calculator.py
+++ TP5/test_calculator.py
@@ -1,13 +1,15 @@
 import unittest
 from app import *
 
-def printqueue(liste):
-    noeud = liste.first
-    print(noeud)
+def containsValue(list1, value):
+    n = list1.first
+    while n.value != value:
+        if n.next:
+            n = n.next
+        else:
+            return False
 
-    while noeud.next:
-        noeud = noeud.next
-        print(noeud)
+    return True
 
 class TestCalculator(unittest.TestCase):
 
@@ -26,20 +28,48 @@ class TestCalculator(unittest.TestCase):
 
         self.assertEqual(merged.max_size, queue1.max_size + queue2.max_size)
 
-        print("merged")
-        printqueue(merged)
-        # print("queue1")
-        # printqueue(queue1)
-        # print("queue2")
-        # printqueue(queue2)
+        for i in range(len(queues)):
+            for j in range(queues[i].max_size):
+                self.assertEqual(merged.dequeue(), queues[i].dequeue())
+        
+        
+    def test_union_queue_linkedList(self):
+        calc = Calculator()
+        queue = Queue(4)
+        linked = LinkedList()
 
-        # for i in range(len(queues)):
-        #     for i in range(queues[i].max_size):
-        #         print(merged.dequeue())
-        #         print(queues[i].dequeue())
+        for i in range(1,5):
+            queue.enqueue(i)
+            linked.append(i + 4)  
+
+        merged = calc.union(queue, linked)
+
+        self.assertEqual(merged.size(), queue.max_size + linked.size())
+
+        for i in range(queue.max_size):
+            self.assertTrue(containsValue(merged, queue.dequeue()))
+
+        for i in range(linked.size()):
+            self.assertTrue(containsValue(merged, linked.peek()))
+
+    def test_union_linkedList_linkedList(self):
+        calc = Calculator()
+        linked1 = LinkedList()
+        linked2 = LinkedList()
+
+        for i in range(1,5):
+            linked1.append(i)
+            linked2.append(i + 4)
+
+        lists = [linked1, linked2]
+        merged = calc.union(linked1, linked2)
+
+        self.assertEqual(merged.size(), linked1.size() + linked2.size())
         
-        
-    
+        for i in range(len(lists)):
+            for j in range(lists[i].size()):
+                self.assertTrue(containsValue(merged, lists[i].peek()))
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ TP5/test_screenPrinter.py
@@ -0,0 +1,44 @@
+import unittest, sys, io
+from app import *
+
+class TestScreenPrinter(unittest.TestCase):
+    
+    def setUp(self):
+        self.screenPrinter = ScreenPrinter("screenPrinter")
+        self.linkedList = LinkedList()
+        self.stack = Stack(2)
+        self.queue = Queue(2)
+    
+    def testInit(self):
+        self.assertEqual(self.screenPrinter.name, "screenPrinter")
+
+    def test_visitQueue(self):
+        self.queue.enqueue("value1")
+        self.queue.enqueue("value2")
+
+        sys.stdout = io.StringIO() 
+        self.queue.accept(ScreenPrinter(""))
+        self.assertEqual(sys.stdout.getvalue(), "\n\n|value1|value2|\n\n")
+        sys.stdout = sys.__stdout__  
+
+    def test_visitStack(self):
+        self.stack.push("value1")
+        self.stack.push("value2")
+
+        sys.stdout = io.StringIO() 
+        self.stack.accept(ScreenPrinter(""))
+        self.assertEqual(sys.stdout.getvalue(), "\n\n-------\n   value2   \n-------\n   value1   \n-------\n\n")
+        sys.stdout = sys.__stdout__
+
+    def test_visitLinkedList(self):
+        self.linkedList.append("value1")
+        self.linkedList.append("value2")
+
+        sys.stdout = io.StringIO() 
+        self.linkedList.accept(ScreenPrinter(""))
+        self.assertEqual(sys.stdout.getvalue(), "\n\n(value1,value2)\n\n")
+        sys.stdout = sys.__stdout__
+
+
+if __name__ == '__main__':
+    unittest.main()
