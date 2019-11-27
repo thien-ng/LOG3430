5ced6efd3fa812abf4b162481d4a2e0370853728
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-11-26 22:10:18 -0500

    initial commit

--- TP5/test_app.py
+++ TP5/test_app.py
@@ -13,13 +13,13 @@ from test_filePrinter       import TestFilePrinter
 suite = unittest.TestSuite()
 
 suite.addTest(unittest.makeSuite(TestNode))
-suite.addTest(unittest.makeSuite(TestLinkedList)) # TODO
+suite.addTest(unittest.makeSuite(TestLinkedList))
 suite.addTest(unittest.makeSuite(TestQueue))
 suite.addTest(unittest.makeSuite(TestStack))
 suite.addTest(unittest.makeSuite(TestCalculator))
 suite.addTest(unittest.makeSuite(TestAutoAdaptiveQueue))
 suite.addTest(unittest.makeSuite(TestAutoAdaptiveStack))
 suite.addTest(unittest.makeSuite(TestScreenPrinter))
-suite.addTest(unittest.makeSuite(TestFilePrinter)) # TODO
+suite.addTest(unittest.makeSuite(TestFilePrinter))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
